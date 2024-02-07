d8d24c122084bb4b51f7a3a80c96a40c3e0dae21
mametarou967 <mametarou967@gmail.com>
2024-02-07 21:55:39 +0900

    途中

---
+++ projects/06/MyAssenmbler/MyAssenmbler/Parser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAssenmbler
+{
+    public class Parser
+    {
+        private string _filePath;
+        int index = 0;
+        // ファイルパスを格納するためのプライベート変数
+        // コンストラクタ
+
+        string[] lines;
+
+        public Parser(string filePath)
+        {
+            lines = File.ReadAllLines(filePath);
+        }
+
+        public bool hasMoreCommands() => throw new Exception();
+
+        public void advance() => throw new Exception();
+
+        public string symbol() => throw new Exception(); // Symbolまたは10
+
+        public string dest() => throw new Exception(); // 8
+
+        public string comp() => throw new Exception(); // 28
+
+        public string jump() => throw new Exception(); // 8
+
+    }
+}
--- projects/06/MyAssenmbler/MyAssenmbler/Program.cs
+++ projects/06/MyAssenmbler/MyAssenmbler/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,25 @@ namespace MyAssenmbler
     {
         static void Main(string[] args)
         {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            var addFilePath = currentDirectory + @"\..\..\..\..\add\Add.asm";
+            if (File.Exists(addFilePath))
+            {
+                Console.WriteLine("Current directory: " + addFilePath);
+                try
+                {
+                    var parser = new Parser(addFilePath);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("addが存在しません");
+            }
+            Console.ReadLine();
         }
     }
 }
